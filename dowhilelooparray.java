5a8ecc949f4d4197845e1292108e008e7a865b9e
Amrita MS <55478795+amrita99@users.noreply.github.com>
2021-09-30 14:24:25 -0400

    Create dowhilelooparray.java

---
+++ dowhilelooparray.java
@@ -0,0 +1,11 @@
+public class DoWhileLoopArray{
+    public static void main(String[] args) {
+        int arr[]={2,3,4,7,8,9};
+        int i=0;
+        do {
+            System.out.println(arr[i]);
+            i++;
+
+        }while (i<6);
+    }
+}
